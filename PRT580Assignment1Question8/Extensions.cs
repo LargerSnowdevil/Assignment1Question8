dc3b64c71c5289fd78c1a9d5d2002dab1ccc6e79
LargerSnowdevil <drac1223@hotmail.com>
2023-07-28 09:28:08 +0930

    Added comments to the extension methods

--- PRT580Assignment1Question8/Extensions.cs
+++ PRT580Assignment1Question8/Extensions.cs
@@ -8,6 +8,9 @@ namespace PRT580Assignment1Question8
 {
     public static class Extensions
     {
+        /// <summary>
+        /// Converts a string containing a binary number into an int[] containing the same binary number.
+        /// </summary>
         public static int[] ToBinary(this string s)
         {
             var result = new int[s.Length];
@@ -18,11 +21,17 @@ namespace PRT580Assignment1Question8
             return result;
         }
 
+        /// <summary>
+        /// Converts an int[] containing a binary number into a string containing the same binary number.
+        /// </summary>
         public static string ToReadableBinary(this int[] a)
         {
             return string.Join("", a);
         }
 
+        /// <summary>
+        /// Reverses the order of elements held in an int[].
+        /// </summary>
         public static int[] Flip(this int[] a)
         {
             return a.Reverse().ToArray();
